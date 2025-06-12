81262bf68e27b3fdea8adbe5c64fd50f99580707
Lutfinger <przlutowski@gmail.com>
2025-06-12 22:27:54 +0200

    g

--- core/settings/pre_prod.py
+++ core/settings/pre_prod.py
@@ -9,10 +9,10 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'my_website_dev_db',
-        'USER': 'my_website_dev_user',
-        'PASSWORD': 'Banzai123!',
-        'HOST': '195.238.122.125',
-        'PORT': '5432',
+        # 'NAME': 'my_website_dev_db',
+        # 'USER': 'my_website_dev_user',
+        # 'PASSWORD': 'Banzai123!',
+        # 'HOST': '195.238.122.125',
+        # 'PORT': '5432',
     }
 }
