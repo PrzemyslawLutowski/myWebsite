29acd58cceb5f65da48ec5ad66c7112d5518206e
Przemysław Lutowski <przlutowski@gmail.com>
2025-06-29 14:41:26 +0200

    g

--- core/settings/base.py
+++ core/settings/base.py
@@ -25,8 +25,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG', default=0)
-# ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(" ")
+
+DEBUG = os.environ.get('DEBUG', default=0)
+
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', default=[]).split(' ')
 
 # Application definition
 
@@ -74,16 +76,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.environ.get('POSTGRES_ENGINE', 'django.db.backends.postgresql_psycopg2'),
-#         'NAME': os.environ.get('POSTGRES_DATABASE', 'name'),
-#         'USER': os.environ.get('POSTGRES_USER', 'user'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
-#         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-#         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('POSTGRES_ENGINE', 'django.db.backends.postgresql_psycopg2'),
