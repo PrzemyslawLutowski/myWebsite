b561b74315396e733fd4945bff5d0d7ac41dbb44
Przemysław Lutowski <przlutowski@gmail.com>
2025-11-16 12:00:06 +0100

    g

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from django.shortcuts import render
+#
+# # Create your views here.
+# def index_view(request):
+#     template_name = 'index.html'
+#     return render(request, template_name)
